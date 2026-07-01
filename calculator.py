b57d6260e0de50ec6acd1fb26b5144a4a30ae75b
Gaku <gaku1205k@gmail.com>
2026-07-01 09:18:11 +0900

    機能追加:divide関数を追加

--- calculator.py
+++ calculator.py
@@ -7,8 +7,14 @@ def subtract(a,b):
 def multiply(a,b):
     return a * b
 
+def divide(a,b):
+    if b == 0:
+        return "0で割ることはできません"
+    return a / b
+
 if __name__=="__main__":
     print("計算機アプリ")
     print(add(5,3))
     print(subtract(10,4))
     print(multiply(6,7))
+    print(divide(20,4))
